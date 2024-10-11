40f9d0b0e89dcb3940bdf12863d42d6e4322f0ba
shofinew <shofiullasafayat@gmail.com>
2024-10-12 02:49:19 +0600

    take care

--- editcategory.php
+++ editcategory.php
@@ -11,43 +11,44 @@ $con = mysqli_connect("localhost", "root", "", "dokan");
 </head>
 <body>
       <?php
-      if(isset($_GET['id']))
+      if(isset($_GET['id'])){
           $getid =  $_GET['id'];
 
           $sql = "SELECT * FROM category WHERE CategoryID = $getid";
-
+      
           $exsql = mysqli_query($con, $sql);
           while(  $data = mysqli_fetch_assoc($exsql)){
             $catid = $data['CategoryID'];
             $catname = $data['CategoryNAME'];
             $catdate = $data['CategoryDATE'];
           }
+      }
           if(isset($_GET['CategoryNAME'])){
             $new_categoryname = $_GET['CategoryNAME'];
             $new_categorydate = $_GET['CategoryDATE'];
             $new_categoryid = $_GET['CategoryID'];
             $sql1= "UPDATE category SET 
-            CategoryNAME = ' $new_categoryname',
-            CategoryDATE = ' $new_categorydate' WHERE 
-            CategoryID = ' $new_categoryid'";
+            CategoryNAME = '$new_categoryname',
+            CategoryDATE = '$new_categorydate' WHERE 
+            CategoryID = '$new_categoryid'";
 
             $exsql1= mysqli_query($con, $sql1);
-            if($sql1){
+            if($exsql1){
                   echo "Data updated";
             }
             echo mysqli_error($con);
       }
-          
+   
       
       
       
       ?>
-<form action="addcategory.php" method="GET">
-      <input type="text" name="cname" value="<?php echo $catname  ?>"><br><br>
+<form action="editcategory.php" method="GET">
+      <input type="text" name="CategoryNAME" value="<?php echo $catname  ?>"><br><br>
      
-      <input type="date" name="cdate" value="<?php echo $catdate  ?>"><br><br>
+      <input type="date" name="CategoryDATE" value="<?php echo $catdate  ?>"><br><br>
 
-      <input type="text" name="cid" value="<?php echo $catid  ?>" hidden>
+      <input type="text" name="CategoryID" value="<?php echo $catid  ?>" hidden>
 
     
 
